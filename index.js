e4000d0b33bcff9e327a236674be076b7030926b
Kyle <wilichowskikyle@gmail.com>
2019-12-07 17:40:47 -0600

    fixed mysql timeout issue

--- index.js
+++ index.js
@@ -7,15 +7,12 @@ const app = express();
 const SELECT_ALL_BOOKS_QUERY = 'SELECT * FROM books';
 const SELECT_ALL_EMPLOYEES_QUERY = 'SELECT * FROM employees';
 
-const connection = mysql.createConnection( {
+const pool = mysql.createPool( {
+    connectionLimit : 10,
     host: 'us-cdbr-iron-east-05.cleardb.net',
     user: 'ba74ad5c9d3120',
     password: 'd9cf6ee6',
     database: 'heroku_995327c1b8fd6b9'
-});
-
-connection.connect(err => {
-    if(err) return err;
 });
 
 app.use(cors());
@@ -29,14 +26,18 @@ app.get('/employees/add', (req, res) => {
     
     const INSERT_EMPLOYEE_QUERY = `INSERT INTO Employees Values('${employee_no}', '${name}', '${address}', '${salary}', '${isManager}')`;
     // /add?employee_no=0420&name=Kyle&address=123DankSt&salary=69.42&isManager=false
-    connection.query(INSERT_EMPLOYEE_QUERY, (err, results) => {
-        if(err) return res.send(err)
-        else return res.send('Successfully Added Employee');
+    pool.query(INSERT_EMPLOYEE_QUERY, (err, results) => {
+        if(err) {
+            return res.send(err)
+        }
+        else {
+            return res.send('Successfully Added Employee');
+        }
     });
 })
 
 app.get('/books', (req, res) => {
-    connection.query(SELECT_ALL_BOOKS_QUERY, (err, results) => {
+    pool.query(SELECT_ALL_BOOKS_QUERY, (err, results) => {
         if(err) {
             return res.send(err)
         } else {
@@ -48,12 +49,13 @@ app.get('/books', (req, res) => {
 })
 
 app.get('/employees', (req, res) => {
-    connection.query(SELECT_ALL_EMPLOYEES_QUERY, (err, results) => {
+    pool.query(SELECT_ALL_EMPLOYEES_QUERY, (err, results) => {
         if(err) return res.send(err)
         else return res.json({ data: results })
     })
 })
 
 app.listen(process.env.PORT || 4000, () => {
-    console.log(`products on Heroku PORT`);
+    let port = process.env.PORT ? `Heroku port` : `port 4000`;
+    console.log(`books on ${port}` );
 })
